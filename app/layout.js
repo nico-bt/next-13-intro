15daa30a41847cc23babc5cbc50add01a3a984d9
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-04-08 20:50:01 -0300

    Layout as base for all routes

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,26 @@
+import { Poppins } from "next/font/google"
+import Header from "./components/Header"
 import "./globals.css"
 
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+})
+
 export const metadata = {
-  title: "Next 13",
-  description: "Intro to Next.js version 13",
+  title: "Bat Media",
+  description: "Web development. Simple project to see Nexs.js version 13",
+  keywords:
+    "web development, next, javascript, react, html, css, battaglia, developer, dev",
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.className}>
+        <Header />
+        <main className="container">{children}</main>
+      </body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,24 @@
+import Link from "next/link"
+
 const HomePage = () => {
-  return <div>Homepage</div>
+  return (
+    <>
+      <h1>Welcome To Bat Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet asperiores
+        quia necessitatibus sunt fuga ea nemo? Exercitationem rem distinctio
+        animi iste ad eveniet veniam?
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus fugit
+          quae, quisquam veritatis ut maiores cupiditate. Beatae dolor esse
+          veniam debitis laudantium. Iusto delectus magni sequi nobis odit
+          blanditiis voluptate inventore velit, modi magnam in voluptas!
+          Voluptates soluta optio, nobis rem nesciunt numquam expedita molestiae
+          nam laborum culpa atque magnam.
+        </p>
+      </p>
+    </>
+  )
 }
 
 export default HomePage
