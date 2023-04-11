1470a2afc7eeecb6ede8542d7cf09950fc4c8f55
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-04-10 21:03:01 -0300

    Fix typo in homePage and add lorem in about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,28 @@
 const AboutPage = () => {
-  return <div>About Page</div>
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere non
+        laudantium delectus corporis saepe deserunt accusamus in iusto, odit
+        perferendis earum, ab sunt ipsa.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere non
+        laudantium delectus corporis saepe deserunt accusamus in iusto, odit
+        perferendis earum, ab sunt ipsa.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere non
+        laudantium delectus corporis saepe deserunt accusamus in iusto, odit
+        perferendis earum, ab sunt ipsa. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Dicta sequi repudiandae odit ex, reiciendis, excepturi
+        sunt officia commodi earum a aliquam labore omnis. Perspiciatis quae
+        repellat et nostrum eaque mollitia nobis voluptas quidem, expedita
+        velit, incidunt impedit, quis a veritatis!
+      </p>
+    </div>
+  )
 }
 
 export default AboutPage
--- app/page.js
+++ app/page.js
@@ -8,14 +8,14 @@ const HomePage = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet asperiores
         quia necessitatibus sunt fuga ea nemo? Exercitationem rem distinctio
         animi iste ad eveniet veniam?
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus fugit
-          quae, quisquam veritatis ut maiores cupiditate. Beatae dolor esse
-          veniam debitis laudantium. Iusto delectus magni sequi nobis odit
-          blanditiis voluptate inventore velit, modi magnam in voluptas!
-          Voluptates soluta optio, nobis rem nesciunt numquam expedita molestiae
-          nam laborum culpa atque magnam.
-        </p>
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus fugit
+        quae, quisquam veritatis ut maiores cupiditate. Beatae dolor esse veniam
+        debitis laudantium. Iusto delectus magni sequi nobis odit blanditiis
+        voluptate inventore velit, modi magnam in voluptas! Voluptates soluta
+        optio, nobis rem nesciunt numquam expedita molestiae nam laborum culpa
+        atque magnam.
       </p>
     </>
   )
